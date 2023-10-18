1509147c389e1f095638a3305491a0085e817edf
Eduardo Andrade <ejac09102006@gmail.com>
2023-10-17 23:39:33 -0500

    Programa finalizado con la correcta impresion del resultado

--- Fibonacci.c
+++ Fibonacci.c
@@ -3,8 +3,8 @@
 int main(void){
     int num= 0, nuevo, sum;
     
-    
-        for (nuevo= 1; nuevo <= 10000;){ 
+    printf("%d\n", num);
+        for (nuevo= 1; nuevo <= 11000;){ 
             printf("%d\n", nuevo);
             sum= nuevo + num;
             num= nuevo;
