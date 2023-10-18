0739eb7cc104e257fbffa3037c83bfb44b265d18
Eduardo Andrade <ejac09102006@gmail.com>
2023-10-17 23:01:11 -0500

    Error corregido, correcto funcionamiento y ultima version del programa

--- parejas.c
+++ parejas.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(void){
-    int num1, pareja=0;
+    int num1, pareja=1;
     for (num1= 0; num1 < 10; num1++){
-        
+        printf("%d %d\n", num1, pareja);
         if (num1 % 2 !=0){
             pareja++;
         }
-    printf("%d %d\n", num1, pareja);
+    
     }
     
 
