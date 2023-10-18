23e31f9ed5f83f66b333afd18e68fe1427e86571
Eduardo Andrade <ejac09102006@gmail.com>
2023-10-17 23:00:08 -0500

    Primera version del programa utilizando la funcion for e if anidadas

--- parejas.c
+++ parejas.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(void){
-    int num1, pareja;
+    int num1, pareja=0;
+    for (num1= 0; num1 < 10; num1++){
+        
+        if (num1 % 2 !=0){
+            pareja++;
+        }
+    printf("%d %d\n", num1, pareja);
+    }
+    
+
 }
